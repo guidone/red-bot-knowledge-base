15a2607c0b43e880e912fe09f50eec2d9ae1670e
Guido <guido.bellomo@gmail.com>
2020-06-18 20:52:49 +0200

    first release

--- index.js
+++ index.js
@@ -1,12 +1,50 @@
+import React from 'react';
 import _ from 'lodash';
-
+import { Notification, Button } from 'rsuite';
 import { plug } from 'code-plug';
-import Content from '../../src/components';
+
+import { Content, withConfigurationPage, HelpElements } from '../../src/components';
+import useSocket from '../../src/hooks/socket';
 
 const { Contents, Categories } = Content;
+const { NodeRedNode, SlugHelp, TypeCommand, ChatbotStatus } = HelpElements;
 
-import Configuration from './pages/configuration';
+import ConfigurationForm from './views/form';
 
+const Legend = () => {
+  const { sendMessage } = useSocket();
+  return (
+    <div>
+      <NodeRedNode>Knowledge Base</NodeRedNode>
+      <p>
+        Configure the <b>Knowledge Base</b> block, use the <b>Articles</b> section to enter the knowledge base of your product or
+        service.<br/>
+        For articles that represent the same content but in different languages, use the same <em>slug</em>, the block will take care of
+        the rest.
+      </p>
+      <p>
+        Adjust the sensitivity of the search engine, raise it when the results are not relevant to the user's question, lower it when the
+        search is not returning anything, keep an eye on the system console to see the score of the searches.<br/>
+        With <b>Answer by Default</b> the block can answer automatically to any input text message or can start answering only when the
+        chatbot in <ChatbotStatus>faq.answer</ChatbotStatus> status, which is entered when the user types senteces like <TypeCommand>I need help</TypeCommand>.
+      </p>
+      <SlugHelp/>
+      <div style={{ textAlign: 'center', marginTop: '10px' }}>
+        <Button
+          appearance="primary"
+          onClick={() => {
+            sendMessage('faq.refresh');
+            Notification.success({ title: 'Knowledge Base', description: 'Re-index of articles started' })
+          }}
+          >Refresh index</Button>
+      </div>
+    </div>
+  );
+};
+
+
+
+// sidebar items
 plug(
   'sidebar',
   null,
@@ -22,7 +60,6 @@ plug(
     ]
   }
 );
-
 plug(
   'sidebar',
   null,
@@ -37,7 +74,7 @@ plug(
     ]
   }
 );
-
+// pages
 plug('pages', Categories, {
   url: '/knowledge-base/categories',
   title: 'Categories',
@@ -49,12 +86,20 @@ plug('pages', Categories, {
     'Categories'
   ]
 });
-plug('pages', Configuration, {
-  url: '/knowledge-base/configure',
-  title: 'Configure',
-  id: 'faqs-configure',
-  permission: ['faq.configure', 'configure'],
-});
+plug(
+  'pages',
+  withConfigurationPage(
+    'knowledge-base',
+    ConfigurationForm,
+    { Legend, title: 'Knowledge Base' }
+  ),
+  {
+    url: '/knowledge-base/configure',
+    title: 'Configure',
+    id: 'faqs-configure',
+    permission: ['faq.configure', 'configure'],
+  }
+);
 plug('pages', Contents, {
   url: '/knowledge-base',
   title: 'Knowledge Base',
@@ -66,7 +111,7 @@ plug('pages', Contents, {
     saveContent: 'Save article'
   }
 });
-
+// permissions
 plug(
   'permissions',
   null,
